e2418e97b9085677013baeb2661b744c1b3a13a6
hrishikes23 <hrishikesh23@live.com>
2018-05-26 22:43:36 +0530

    Print Right angle triangle using iterators

---
+++ Codility/Iterations/task-2/task.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+// Program to pring Right Angle triangle using iterators
+
+int main()
+{
+
+ int rows = 0,col = 0,tot_rows=5;
+ 
+  
+  for(rows=1;rows<=5;rows++)
+  {
+
+    for(col=1;col<=rows;col++)
+    {
+ 	printf("*");
+    }
+
+  printf("\n");
+  }
+
+  
+
+
+ return 0;
+}
